fe699b63dbaa959845dfb993478498a96fd00ea3
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-01-10 11:32:22 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,105 @@
+"use strict";
+
+const billInput = document.querySelector("#billValue");
+const customTipInput = document.querySelector(".custom-tip-input");
+const numberOfPeopleInput = document.querySelector("#number-0f-people-input");
+const tipAmountValue = document.querySelector(".tip-value");
+const totalValue = document.querySelector(".total-value");
+const btnReset = document.querySelector(".btn-reset");
+const selectTipBtns = document.querySelectorAll(".select-tip-percentage");
+
+btnReset.addEventListener("click", (event) => {
+  // Reset the inputs and values
+  billInput.value = "";
+  customTipInput.value = "";
+  numberOfPeopleInput.value = "";
+  tipAmountValue.innerHTML = "$0.00";
+  totalValue.innerHTML = "$0.00";
+  // Reset the tip percentage btns
+  selectTipBtns.forEach((tipBtn) => {
+    tipBtn.classList.remove("active");
+  });
+  //Default 15%
+  selectTipBtns[2].classList.add("active");
+});
+
+// Make the tip percentage btns clickable
+selectTipBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    selectTipBtns.forEach((tipBtn) => {
+      tipBtn.classList.remove("active");
+    });
+    if (event.target.classList.contains("custom-tip-input")) {
+      event.target.parentElement.classList.add("active");
+    } else {
+      event.target.classList.add("active");
+    }
+    calculateTip();
+  });
+  selectTipBtns[2].classList.add("active");
+});
+
+const calculateTip = () => {
+  const billValue = parseFloat(billInput.value);
+  const numberOfPeople = parseFloat(numberOfPeopleInput.value);
+  const customTipActive = document.querySelector(".select-tip-custom.active");
+  let tipPercentage = parseInt(
+    document.querySelector(".select-tip-percentage.active").dataset.percentage
+  );
+
+  if (customTipActive) {
+    tipPercentage = parseFloat(
+      document.querySelector(".custom-tip-input").value
+    );
+  }
+
+  const totalAmount = parseFloat((tipPercentage / 100) * billValue).toFixed(2);
+  const tipAmount = parseFloat(totalAmount / numberOfPeople).toFixed(2);
+  const actualTotalAmount = parseFloat(
+    (billValue + parseFloat(totalAmount)) / numberOfPeople
+  ).toFixed(2);
+  tipAmountValue.innerHTML = `$${tipAmount}`;
+  totalValue.innerHTML = `$${actualTotalAmount}`;
+};
+
+const isNumber = (value) => {
+  // Allow Exceptions
+  if (
+    value === "Backspace" ||
+    value === "ArrowLeft" ||
+    value === "ArrowRight" ||
+    value === "."
+  ) {
+    return true;
+  }
+  const regex = /^[0-9]+$/;
+  return regex.test(value);
+};
+
+// Calculate the tip
+billInput.addEventListener("keyup", (event) => {
+  calculateTip();
+});
+customTipInput.addEventListener("keyup", (event) => {
+  calculateTip();
+});
+numberOfPeopleInput.addEventListener("keyup", (event) => {
+  calculateTip();
+});
+
+// Prevent alphabetical characters from being entered
+billInput.addEventListener("keydown", (event) => {
+  if (!isNumber(event.key)) {
+    event.preventDefault();
+  }
+});
+customTipInput.addEventListener("keydown", (event) => {
+  if (!isNumber(event.key)) {
+    event.preventDefault();
+  }
+});
+numberOfPeopleInput.addEventListener("keydown", (event) => {
+  if (!isNumber(event.key)) {
+    event.preventDefault();
+  }
+});
